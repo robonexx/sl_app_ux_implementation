841b605e023cf8630780a2dbd65260e7ce999901
robonexx <robertwagar@gmail.com>
2021-10-02 16:10:53 +0200

    set in counter, need to change it to a ternary operator with additional state changes, did a little cheat for now, need to fix it next

--- src/pages/confirmpage/ConfirmPage.js
+++ src/pages/confirmpage/ConfirmPage.js
@@ -2,6 +2,52 @@ import React, { useState } from 'react';
 import './ConfirmPage.css'
 
 const ConfirmTickets = () => {
+    const [count, setCount] = useState(0)
+    const [sum, setSum] = useState(0)
+    const [redCount, setRedCount] = useState(0)
+    const [redSum, setRedSum] = useState(0)
+
+    let fullPrice = 38
+    let reducedPrice = 25
+    
+    const increment = () => {
+        setCount(count + 1)
+        setSum((count + 1) * fullPrice)
+    }
+    let decrement = () => {
+        setCount(count - 1)
+        setSum((count -1) * fullPrice)
+    }
+    if (count <= 0) {
+        decrement = () => {
+            setCount(0)
+        }
+        
+    }
+    const incrementRed = () => {
+        setRedCount(redCount + 1)
+        setRedSum((redCount + 1) * reducedPrice)
+    }
+    let decrementRed = () => {
+        setRedCount(redCount - 1)
+        setRedSum((redCount -1) * reducedPrice)
+    }
+    if (redCount <= 0) {
+        decrementRed = () => {
+            setRedCount(0)
+        }
+        
+    }
+    
+
+    
+
+
+
+    
+    
+    
+
     return (
         <div className="wrapper">
             <header className="confirm-header">
@@ -20,22 +66,26 @@ const ConfirmTickets = () => {
             <section className="confirm-section">
                 <h3>Antal vuxen</h3>
                 <div className="buttons">
-                    <button className="btn btn-minus">-</button>
-                    <span>1</span>
-                    <button className="btn btn-plus">+</button>
+                    <button className="btn btn-minus"
+                    onClick={decrement}>-</button>
+                    <span>{count}</span>
+                    <button className="btn btn-plus"
+                    onClick={increment}>+</button>
                 </div>
             </section>
             <section className="confirm-section">
                 <h3>Antal rabaterat</h3>
                 <div className="buttons">
-                    <button className="btn btn-minus">-</button>
-                    <span>1</span>
-                    <button className="btn btn-plus">+</button>
+                    <button className="btn btn-minus reduced"
+                    onClick={decrementRed}>-</button>
+                    <span>{redCount}</span>
+                    <button className="btn btn-plus reduced"
+                    onClick={incrementRed}>+</button>
                 </div>
             </section>
             <section className="confirm-section">
                 <h3>Att betala</h3>
-                <h3>38 kr</h3>
+                <h3>{redSum} kr</h3>
             </section>
             <div className="pay-btn-div">
                 <button className="pay-btn">Fortsätt till betalning</button>
