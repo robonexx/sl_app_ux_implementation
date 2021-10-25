0d1dcbfd12e89b008c2a726fbc7832ea10a193c0
robonexx <robertwagar@gmail.com>
2021-10-25 12:00:26 +0200

    changed button to styled components. Ill change the styling to styled components on the projects for easier use and to hinder css mess

--- src/components/ui/buttons/BookBikeBtn.js
+++ src/components/ui/buttons/BookBikeBtn.js
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './bookBikeBtn.css'
-import ElScooter from '../icons/Scooter';
-import Bike from '../icons/Bike';
+import styled from 'styled-components'
+
+const BookBtn = styled.button`
+    width: 188px;
+    height: 42px;
+    border: none;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    color: #fff;
+    text-align: center;
+    /* background: #20252c; */
+`
+
+const BtnsWrapper = styled.div`
+    width: 375px;
+    height: 220px;
+    position: absolute;
+    bottom: 8vh;
+    z-index: 100;
+`
+const ButtonToggle = styled(BookBtn)`
+  background: #4c5157;
+  ${({ active }) =>
+    active &&
+    `
+    background: #20252c;
+  `}
+`;
+const BtnGroup = styled.div`
+    width: 100%;
+        display: flex;
+`
+
+
+
+
+const types = ['Bike', 'Scooter'];
 
 const BookBikeBtn = () => {
+    const [active, setActive] = useState(types[0])
 
-    const handleClick = (e)=>{
+    /* const handleClick = (e)=>{
         console.log("this is working fine");
         e.preventDefault();
         e.target.style.background = 'var(--color-neutral-700)'
         console.log(e.target);
-    }
+    } */
     return (
-        <div className="bike-btns-wrapper">
-            <div className="bike-btn-container">
-                <button className="vehicle-btn book-bike">
-                    Cykel
-                    {/* <Bike /> */}
-                </button>
-                <button onClick={handleClick} className="vehicle-btn book-scooter">
-                    Scooter
-                   {/*  <ElScooter  /> */}
-                </button>
-            </div>
+        
+        <BtnsWrapper >
+            <BtnGroup>
+                {types.map(type => (
+                   
+                    <ButtonToggle
+                        key={type}
+                        active={active === type}
+                        onClick={() => setActive(type)}
+                    >
+                        {type}
+                    </ButtonToggle>
+               ))}
+            </BtnGroup>
             <div className="search-add bottom-container">                
                 <div className="booking-info">
                     <h2>Din cykel är nu bokad</h2>
@@ -30,7 +69,8 @@ const BookBikeBtn = () => {
                     <span>Lås upp cykel med Qr koden på din biljett</span>
                 </div>
             </div>
-        </div>
+            </BtnsWrapper>
+            
      );
 }
  
