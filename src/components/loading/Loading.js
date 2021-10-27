409aa3bf71efde987d9f074e4e33ca4f6ad93027
robonexx <robertwagar@gmail.com>
2021-10-27 10:24:17 +0200

    changed the animation without path fill on the svg

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -3,30 +3,12 @@ import LogoLoading from './LogoLoading';
 import './loading.scss'
 import { motion } from 'framer-motion'
 
-const loadVariants = {
-    initial: {
-        opacity: 0,
-
-    },
-    visible: {
-        opacity: 1,
-        transition: {
-            duration: 0.4
-        }
-    }
-}
-
 const Loading = ({loading, setLoading}) => {
     
 
     return (
         
-        <div className="loading"
-            variants={loadVariants}
-            initial="hidden"
-            animate="visible"
-               
-            >
+        <div className="loading">
             <LogoLoading loading={loading} setLoading={setLoading} />
             </div>
             
